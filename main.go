2f553b3f95f5d6e539776a3af2d2529cbc8cd580
Luiz cunha <luiz.cunha@codeandsoul.com>
2023-10-10 16:33:58 -0300

    Add router and routes

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/felipeitap/go-oportunities/router"
+
+func main(){
+
+	router.Initialize()
+}
--- router/router.go
+++ router/router.go
@@ -1,3 +1,14 @@
 package router
 
-func initialize
+import (
+
+	"github.com/gin-gonic/gin"
+)
+
+func Initialize(){
+	router := gin.Default()
+	
+	initializeRoutes(router)
+
+	router.Run("localhost:8080")
+}
